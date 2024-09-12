27c9b4c97ea3c15a9f71e574d00241f31154aa3c
Heisenberg688 <114392080+Heisenberg688@users.noreply.github.com>
2024-09-12 11:23:21 +0200

    Plugins Wordpress 1.0

---
+++ theme-listing/theme-listing.php
@@ -0,0 +1,53 @@
+<?php
+
+    // Dans le dossier wp-content/plugins créer un dossier 'theme-listing'
+
+    // Dans le dossier 'theme-listing' créer un fichier PHP 'theme-listing.php'
+
+
+/*
+
+Plugin Name: Theme Listing Plugin 
+Description: A simple plugin to list all themes directories 
+Version: 1.0
+Author: IUT MULHOUSE
+
+*/
+
+if(!defined("ABSPATH")){
+    exit; 
+}
+
+
+function theme_listing_page() {
+    echo"<div>
+            <h1>Ma page dans l'admin</h1>
+        </div>"; 
+
+
+}
+
+
+function theme_listing_menu(){
+    add_menu_page('Titre page',  'Titre menu','manage_options','idmenu','theme_listing_page',
+    'dashicons-admin-appearance', 5);
+    
+}
+
+add_action('admin_menu', 'theme_listing_menu'); 
+
+
+
+function liste_theme_dir(){
+
+    //Lis le dossier de theme WP
+
+    $themesdir = scandir(get_theme_root());
+
+    echo "<h2>Liste de tous les themes</h2>";
+
+    echo'<ul>'
+
+}
+
+?>
